9fc0863c002b8d294e4332ad40f83ba530e890ba
Logan Flory <logan.flory@gmail.com>
2021-02-05 12:26:47 -0500

    Implemented ISprite interface

---
+++ testMonogame/testMonogame/ISprite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace testMonogame
+{
+    interface ISprite
+    {
+        public void Draw(SpriteBatch spriteBatch);
+        public void Update(Game1 game);
+    }
+}
