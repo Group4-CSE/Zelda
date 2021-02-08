05efb13fd14f5a6c1b9b87a4698f9f75a3566fda
Tyler Liu <42123666+TylerLiu88@users.noreply.github.com>
2021-02-08 11:30:16 -0500

    Implemented Enemy Interface

---
+++ testMonogame/testMonogame/IEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace testMonogame
+{
+    public interface IEnemy
+    {
+        //Update the Enemy Sprite
+        public void Update();
+        //Update enemy movements (Maybe use vectors, so will not be void)
+        public void Move();
+        //Get the HP to see how many more attacks left of the player
+        public void getHP();
+        //Attack the player
+        public void Attack();
+    }
+}
