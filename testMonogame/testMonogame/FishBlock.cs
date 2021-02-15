28e98c453b9e3f30bb11a2f713b0f9e3a78b5213
Tyler Liu <42123666+TylerLiu88@users.noreply.github.com>
2021-02-15 11:34:50 -0500

    Removed Blocks Back

---
+++ testMonogame/testMonogame/FishBlock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace testMonogame
+{
+    class FishBlock : IObject, ISprite
+    {
+
+        Texture2D texture;
+        Rectangle destRect;
+        int x;
+        int y;
+
+        Rectangle sourceRect = new Rectangle(35, 0, 51, 16);
+        Color color = Color.Green;
+
+        public FishBlock(Texture2D incTexture, Vector2 pos)
+        {
+            texture = incTexture;
+            x = (int)pos.X;
+            y = (int)pos.Y;
+            destRect = new Rectangle(x, y, 16, 16);
+        }
+
+        public void Update(Game1 game)
+        {
+            // COLLISION WILL GO HERE
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, destRect, sourceRect, color);
+        }
+
+        public void Interact(IPlayer player)
+        {
+            // NULL
+        }
+    }
+}
