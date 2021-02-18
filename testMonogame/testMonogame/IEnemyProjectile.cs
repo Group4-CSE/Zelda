9dc8ab136ee6a8ddaacc37b96ea561f02ee45319
Logan Flory <logan.flory@gmail.com>
2021-02-17 23:34:29 -0500

    Implemented IEnemyProjectile

---
+++ testMonogame/testMonogame/IEnemyProjectile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testMonogame
+{
+    interface IEnemyProjectile
+    {
+        public void Move();
+        public void doDamage(IPlayer player);
+        public void delete();
+    }
+}
