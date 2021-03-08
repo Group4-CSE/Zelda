ca3825f64c4a19eb8dcffd0dad74b1fc7b09c74b
qiujerry <qiujer5687@gmail.com>
2021-03-08 10:21:00 -0500

    start game manager class

---
+++ testMonogame/testMonogame/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace testMonogame
+{
+    public class GameManager
+    {
+        public GameManager()
+        {
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
