78b49a7087a1eb6806f2ef5fb067f31f6d9fefa3
Logan Flory <logan.flory@gmail.com>
2021-03-09 22:26:24 -0500

    Fixed SwordPlayerProjectile to be compatible with collision updating. Implemented PlayerprojectileBlock detection / handling

---
+++ testMonogame/testMonogame/PlayerprojectileBlockCollision.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace testMonogame
+{
+    class PlayerprojectileBlockCollision
+    {
+        Rectangle projectileRect;
+        Rectangle blockRect;
+        Rectangle collision;
+        int xCollisionSize;
+        int yCollisionSize;
+        public void detectCollision(List<IPlayerProjectile> projectiles, List<IObject> blocks, Game1 game)
+        {
+            foreach (var projectile in projectiles)
+            {
+                projectileRect = projectile.getDestRect();
+                foreach (var block in blocks)
+                {
+                    blockRect = block.getDestRect();
+                    collision = Rectangle.Intersect(projectileRect, blockRect);
+                    if (!collision.IsEmpty) handleCollision(projectile, game);
+                }
+            }
+        }
+
+        public void handleCollision(IPlayerProjectile projectile, Game1 game)
+        {
+            projectile.delete(game);
+        }
+    }
+}
