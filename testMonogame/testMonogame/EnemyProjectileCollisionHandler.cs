b0260b2e5fa4f6acb0dbaf607254d8975748605a
qiujerry <qiujer5687@gmail.com>
2021-03-12 14:38:59 -0500

    class for enemy projectile collision handling

---
+++ testMonogame/testMonogame/EnemyProjectileCollisionHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+namespace testMonogame
+{
+    public class EnemyProjectileCollisionHandler
+    {
+        Game1 game;
+        public EnemyProjectileCollisionHandler(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void handlePlayerCollision(IEnemyProjectile proj, IPlayer player)
+        {
+            //test for collision
+            Console.WriteLine("proj:" + proj.getDestRect().ToString());
+            Console.WriteLine("player: " + player.getDestRect().ToString());
+            if (player.getDestRect().Intersects(proj.getDestRect()))
+            {
+                proj.doDamage(player);
+                proj.delete(game);
+            }
+
+            
+        }
+    }
+}
